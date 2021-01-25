427ce3dd7975561afbb87af56b2276d0c0a40f7a
Jianli-Huang <huangjianli_st@163.com>
2021-01-25 16:06:05 +0800

    first commit

---
+++ Assets/_Scripts/GestureEvents.cs
@@ -0,0 +1,32 @@
+﻿using HedgehogTeam.EasyTouch;
+using UnityEngine;
+
+public class GestureEvents : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        //初始化 Animator 组件，用户切换动画
+        animator = GetComponent<Animator>();
+    }
+
+    //在脚本被激活时，将切换动画的事件绑定到 Tap 手势上
+    private void OnEnable()
+    {
+        EasyTouch.On_SimpleTap += NextAnimationEvent;
+    }
+
+    //在脚本被关闭时，移除 Tap 手势上的切换动画事件
+    private void OnDisable()
+    {
+        EasyTouch.On_SimpleTap -= NextAnimationEvent;
+    }
+
+    //切换动画的具体事件
+    private void NextAnimationEvent(Gesture gesture)
+    {
+        animator.SetTrigger("Next");
+
+    }
+}
---
+++ Assets/_Scripts/TouchEvents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchEvents : MonoBehaviour
+{
+
+	private Animator animator;
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+}
